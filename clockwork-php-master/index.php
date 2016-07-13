39b3e9b296cfb11ff85d8292c0c0bb94d905d7b2
Juan Carlos <juancagb.17@gmail.com>
2016-07-13 10:37:18 -0500

    Testing 4

--- clockwork-php-master/index.php
+++ clockwork-php-master/index.php
@@ -7,13 +7,12 @@ $clockwork = new Clockwork($apikey);
 
 $query = 'select * from USUARIO where IDUsuario=1';
 $registros = sqlsrv_query($conexion, $query);
-echo 'Se enviarán mensajes a: ';
+// echo 'Se enviarán mensajes a: ';
 while ($row = sqlsrv_fetch_array($registros))
 {
 	$message = array('to'=> $row['Celular'], 'message' => '¡ALERTA! intruso en casa, revisa la aplicacion');
-	echo $row['Celular'];
 }
 
-// $done = $clockwork->send($message);
+$done = $clockwork->send($message);
 
 ?>
