42034f03d8081295219d998c6a9ec1c8ceb0b0de
Juan Carlos <juancagb.17@gmail.com>
2016-07-12 23:20:07 -0500

    JoAn files

---
+++ clockwork-php-master/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require("class-Clockwork.php");
+require("conexion.php");
+$apikey = "81527975d971c80cacc9e5970249e7c17d4c0804";
+$clockwork = new Clockwork($apikey);
+
+$query = 'select * from USUARIO where IDUsuario=1';
+$registros = sqlsrv_query($conexion, $query);
+while ($row = sqlsrv_fetch_object($registros))
+{
+
+$message = array('to'=> $row[4], 'message' => '¡ALERTA! intruso en casa, revisa la aplicacion');
+}
+$done = $clockwork->send($message);
+
+
+?>
