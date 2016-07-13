37cf6ccbeea8955b299c1f4dcafe367eae7fffe5
Juan Carlos <juancagb.17@gmail.com>
2016-07-12 20:49:55 -0500

    Incidencias

---
+++ incidencias.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Este WS nos permite apagar el sensor usando SP_ActualizarN.
+*/
+	require 'conexion.php';
+	header('Content-Type: application/json');
+
+	$sql_SP = "{ CALL select_incidencias() }";
+
+	$params = [
+	];
+
+	$result_set = sqlsrv_query( $conexion, $sql_SP, $params );
+	if( $result_set === false ) {
+		$data['exito'] = false;
+		$data['mensaje'] = sqlsrv_errors()[0]['mensaje'];
+		die( json_encode($data) );
+	}
+	
+	echo resultSetToJson('incidencias', $result_set);
